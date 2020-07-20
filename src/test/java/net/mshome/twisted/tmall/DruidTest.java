a1311ac1dcaee6d328dfb88c6c5ca2016b1c76f3
tangjizhou <tangjizhouchn@foxmail.com>
2020-07-20 23:36:10 +0800

    jdk14 switch

--- src/test/java/net/mshome/twisted/tmall/DruidTest.java
+++ src/test/java/net/mshome/twisted/tmall/DruidTest.java
@@ -4,15 +4,38 @@ import com.alibaba.druid.filter.config.ConfigTools;
 import org.junit.Test;
 
 /**
+ * druid密码加密
+ *
  * @author tangjizhouchn@foxmail.com
- * @date 2019/9/1
- * @description druid密码加密
+ * @since 2019/9/1
  */
 public class DruidTest {
 
     @Test
     public void generateEncryptPasswordTest() throws Exception {
         String password = "tmallpass";
+        switch (password) {
+            case "1" -> System.out.println(1231231);
+            case "2" -> System.out.println(1231231);
+            case "3" -> System.out.println(1231231);
+            case "4" -> System.out.println(1231231);
+            case "5", "6" -> System.out.println(1231231);
+            default -> System.out.println("default");
+        }
+
+        String result = switch (password) {
+            case "1" -> "1";
+            case "2" -> "1";
+            case "3" -> "1";
+            case "4" -> "1";
+            case "5", "6" -> "12";
+            default -> {
+                System.out.println(password);
+                yield "default";
+            }
+        };
+
+
         ConfigTools.main(new String[]{password});
     }
 
