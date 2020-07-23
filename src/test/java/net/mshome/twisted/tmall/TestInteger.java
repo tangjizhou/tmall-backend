763770e737068675d258988ae1394abf09c82e14
tangjizhou <tangjizhouchn@foxmail.com>
2020-07-23 23:41:30 +0800

    测试编译优化

--- src/test/java/net/mshome/twisted/tmall/TestInteger.java
+++ src/test/java/net/mshome/twisted/tmall/TestInteger.java
@@ -25,6 +25,29 @@ public class TestInteger {
         System.out.println(g.equals(a + b));// false
 
 
+        if (true) {
+            System.out.println(1);
+        } else {
+            System.out.println(2);
+        }
+
+        // output class file: 包装类型equals不会做类型转换，在算术运算符的作用下会自动拆箱， -128 ～ 127 存在常量缓存
+
+        // Integer a = 1;
+        // Integer b = 2;
+        // Integer c = 3;
+        // Integer d = 3;
+        // Integer e = 321;
+        // Integer f = 321;
+        // Long g = 3L;
+        // System.out.println(c == d);
+        // System.out.println(e == f);
+        // System.out.println(c == a + b);
+        // System.out.println(c.equals(a + b));
+        // System.out.println(g == (long)(a + b));
+        // System.out.println(g.equals(a + b));
+        // System.out.println(1);
+
     }
 
 }
