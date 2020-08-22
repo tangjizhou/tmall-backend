5375cb5cde2cb3bcbcbe100bc8f9263fddbb84b2
tangjizhou <tangjizhouchn@foxmail.com>
2020-08-23 00:13:57 +0800

    更改版本 支持java14

--- src/main/java/net/mshome/twisted/tmall/TmallApplication.java
+++ src/main/java/net/mshome/twisted/tmall/TmallApplication.java
@@ -1,12 +1,13 @@
 package net.mshome.twisted.tmall;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * @author tangjizhouchn@foxmail.com
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class TmallApplication {
 
     public static void main(String[] args) {
